5e7ad6ce2b9f9e1777f58e573c346a7dedffa248
Vnictros240 <Vnictros240@users.noreply.github.com>
2019-09-26 12:17:15 -0700

    Adding Functionality
    Hi Kirk, 
    I turned the variable "commands" into a LIST. 
    I built a function called "showCommands" with two input variables called, "profile" and "cmds"
    
    When the function runs it needs the "net_connect" as the "profile" input and the LIST "commands" as the input for the "cmds" variable. 
    Inside the function "showCommands" there is a FOR loop that will rotate through each item in the LIST "commands" and it will PRINT that to screen. 
    
    After the FOR loop is finished, it will issue the disconnect function and the program will terminate.

--- examples/use_cases/case4_show_commands/send_command.py
+++ examples/use_cases/case4_show_commands/send_command.py
@@ -10,11 +10,27 @@ cisco1 = {
 }
 
 net_connect = Netmiko(**cisco1)
-command = "show ip int brief"
 
-print()
-print(net_connect.find_prompt())
-output = net_connect.send_command(command)
-net_connect.disconnect()
-print(output)
-print()
+# Create a List of Cisco commands that can be used later in a 'FOR' loop
+commands = [
+"terminal length 0",
+"show ip int brief",
+"show version",
+"show vlan brief",
+"teminal length 24",
+]
+
+# Create a function with two required inputs called  that will used repeatably 
+def showCommands(profile, cmds):
+    for items in cmds:
+        print(profile.find_prompt())
+        output = profile.send_command(items)
+        print(output + "\n\n")
+    profile.disconnect()
+
+
+
+# By calling the function, with the required inputs "profile" and "cmds"
+# This function will start the "FOR" loop and execute the "show commands"
+#     in the LIST called, "commands".
+showCommands(net_connect, commands)
